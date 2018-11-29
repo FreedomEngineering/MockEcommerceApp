f379f3b50905f0fccb8cc5095a65c78730c7921d
FreedomEngineering <freedomengineering42@gmail.com>
2018-11-28 17:19:31 -0800

    adding react

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const greeting = "hello from javascript";
 
 console.log(greeting);
 
-document.getElementsByTagName('button')[0].addEventListener('click', () => {
-  console.log('OMG CLICKED!!');
-  document.getElementsByTagName('h1')[0].innerText = greeting;
-})
+document.getElementsByTagName("button")[0].addEventListener("click", () => {
+  console.log("OMG CLICKED!!");
+  document.getElementsByTagName("h1")[0].innerText = greeting;
+});
+
+const Root = () => {
+  return <h1>Whoa</h1>;
+};
+
+ReactDOM.render(<Root />, document.getElementById("root"));
