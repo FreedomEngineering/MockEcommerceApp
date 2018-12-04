a7ec7e0058f0c4d97d5db7106e6d2343e95efed0
FreedomEngineering <freedomengineering42@gmail.com>
2018-12-03 21:59:43 -0800

    adding jumbotron

--- src/components/Callout.js
+++ src/components/Callout.js
@@ -2,8 +2,14 @@ import React from "react";
 
 const Callout = () => {
   return (
-    <div id="largeCallout">
-      <h1>LargeCallout</h1>
+    <div id="largeCallout" className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-3">Fluid jumbotron</h1>
+        <p className="lead">
+          This is a modified jumbotron that occupies the entire horizontal space
+          of its parent.
+        </p>
+      </div>
     </div>
   );
 };
