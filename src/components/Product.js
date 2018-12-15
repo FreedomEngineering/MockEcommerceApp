fd1b97dac84096151cddb63603e47257a0c1ea74
FreedomEngineering <freedomengineering42@gmail.com>
2018-12-15 12:04:42 -0800

    add product objects and props to components

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,22 +2,20 @@ import React from "react";
 
 import { ModalButton } from "./Modal";
 
-const Product = () => {
+const Product = props => {
+  const { image, name, price, shortDescription } = props;
   return (
     <div className="card product">
       <div className="card-body">
         <img
           className="shadow rounded img-thumbnail"
-          src="https://via.placeholder.com/200"
+          src={image}
           alt="Card image cap"
         />
         <hr />
-        <h5 className="card-title">Card title</h5>
-        <h6 className="card-subtitle mb-2 text-muted">Price</h6>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <h5 className="card-title">{name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{price}</h6>
+        <p className="card-text">{shortDescription}</p>
         <ModalButton />
       </div>
     </div>
--- src/components/ProductsArea.js
+++ src/components/ProductsArea.js
@@ -2,12 +2,32 @@ import React from "react";
 
 import Product from "./Product";
 
+const hdmiCable = {
+  name: "HDMI Cable",
+  image: "https://via.placeholder.com/200",
+  price: "10.99",
+  shortDescription: "A useful cable",
+  longDescription: "This cable is HDMI. It is very useful. So great, much fun."
+};
+
 const ProductsArea = () => {
   return (
     <div id="productsArea" className="container">
       <div className="card-columns">
-        <Product />
-        <Product />
+        <Product
+          name={hdmiCable.name}
+          image={hdmiCable.image}
+          price={hdmiCable.price}
+          shortDescription={hdmiCable.shortDescription}
+          longDescription={hdmiCable.longDescription}
+        />
+        <Product
+          name="Speakers"
+          image="https://via.placeholder.com/200"
+          price="29.97"
+          shortDescription="A set of nice speakers"
+          longDescription="Speakers are great and make sounds louder. Great value, such sound."
+        />
         <Product />
         <Product />
         <Product />
