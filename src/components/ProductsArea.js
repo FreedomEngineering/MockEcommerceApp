fc29afa98b700a42a2cafd3e8253d49104321aee
FreedomEngineering <freedomengineering42@gmail.com>
2018-12-20 16:41:09 -0800

    populating the products data

--- src/components/ProductsArea.js
+++ src/components/ProductsArea.js
@@ -1,44 +1,14 @@
 import React from "react";
-import { v4 } from "uuid";
 
 import Product from "./Product";
-
-const productsList = [
-  {
-    id: v4(),
-    name: "HDMI Cable",
-    image: "https://via.placeholder.com/200",
-    price: "10.99",
-    shortDescription: "A useful cable",
-    longDescription:
-      "This cable is HDMI. It is very useful. So great, much fun."
-  },
-  {
-    id: v4(),
-    name: "Speakers",
-    image: "https://via.placeholder.com/200",
-    price: "29.97",
-    shortDescription: "A set of nice speakers",
-    longDescription:
-      "Speakers are great and make sounds louder. Great value, such sound."
-  }
-];
+import productsList from "../../lib/products";
 
 const ProductsArea = () => {
   return (
     <div id="productsArea" className="container">
       <div className="card-columns">
         {productsList.map(product => {
-          return (
-            <Product
-              key={product.id}
-              name={product.name}
-              image={product.image}
-              price={product.price}
-              shortDescription={product.shortDescription}
-              longDescription={product.longDescription}
-            />
-          );
+          return <Product key={product.id} {...product} />;
         })}
       </div>
     </div>
